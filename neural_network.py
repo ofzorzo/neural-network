b64839d0aa9d4273a81647cc81bc87c5a47cca2c
Felipe Zorzo Pereira <felipe.1707@hotmail.com>
2019-06-12 19:42:27 -0300

    Numerical verification implemented

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import copy
 
 def g(x):
     return np.matrix([1 / ( 1 + math.exp( -i ) ) for i in x]).transpose()
@@ -16,7 +17,7 @@ def element_wise_mult(m1, m2):
 
 
 class NeuralNetwork:
-    def __init__(self, struct, weights):
+    def __init__(self, struct, weights, epsilon):
         self.thetas = weights
         self.struct = struct
         self.a = self.init_activations()
@@ -24,6 +25,9 @@ class NeuralNetwork:
         self.layers = len(self.struct['neurons'])
         self.alpha = 0.1
         self.max_iterations = 1
+        self.thetas_numerical = copy.deepcopy(self.thetas)
+        self.a_numerical = copy.deepcopy(self.a)        
+        self.epsilon = epsilon
 
     def init_activations(self):
         activations = []
@@ -41,7 +45,7 @@ class NeuralNetwork:
 
     # Considera um conjunto de treinamento formado por uma lista de pares do tipo:
     # (entradas, saídas esperadas), onde ambos os elementos são listas de valores inteiros
-    def backpropagation (self, train):
+    def backpropagation(self, train):
         iter = 0
         prev_theta = None
         while (not self.stop_condition(iter, prev_theta)):
@@ -97,17 +101,19 @@ class NeuralNetwork:
             
             for l in range(len(Grad)):
                 print ("Gradientes acumulados (com regularização) para theta" + str(l+1) + ":\n" + str(Grad[l]))
-            
 
             for k in range(self.layers-2, -1 , -1):
                 self.thetas[k] = np.matrix(self.thetas[k]) - np.matrix(self.alpha * Grad[k])
 
             for t in range (len(self.thetas)):
                 print ("Novo theta " + str(t+1) +" com alpha " + str(self.alpha) + ":\n" + str(self.thetas[t]))
+        
+            print("\n--------------------------------------------")
+        return Grad
             
 
     # obtém saídas correspondentes a uma entrada
-    def propagate (self, instance):
+    def propagate(self, instance):
         # adiciona bias
         self.a[0] = np.matrix([1] + instance).transpose() # valores de entrada para a camada sendo 
                                                             # a entrada da rede na primeira camada ou
@@ -119,8 +125,84 @@ class NeuralNetwork:
                
         self.a[self.layers-1] = g(self.thetas[self.layers - 2] * self.a[self.layers - 2])         # (-2) pois existe uma tabela de thetas a menos que camada 
         return self.a[self.layers-1]
+
+    def compute_numerical_verification(self, train):
+        print("Rodando verificacao numerica de gradientes (epsilon=" + "{:.10f}".format(self.epsilon) + ")")
+        gradients = copy.deepcopy(self.thetas_numerical)
+        for k in range(self.layers-2, -1, -1):
+                #print(self.thetas_numerical[k])
+                for i in range(0, len(self.thetas_numerical[k])):
+                    for j in range(0, self.thetas_numerical[k][i].size):
+                        #print(self.thetas_numerical[k][i,j])
+                        Jp = 0
+                        Jn = 0
+                        reg_p, reg_n = self.regularized_terms(k, i, j)
+                        for sample in train:
+                            predicted_p = self.propagate_numerical(sample[0], self.thetas_numerical, k, i, j, "+")
+                            predicted_n = self.propagate_numerical(sample[0], self.thetas_numerical, k, i, j, "-")
+                            expected = np.matrix(sample[1]).transpose()
+                            for out in range(0, len(expected)): # soma os erros das múltiplas saídas
+                                Jp = Jp + ( -expected[out, 0]*(math.log(predicted_p[out, 0])) - (1-expected[out, 0])*(math.log(1-predicted_p[out, 0])) )
+                                Jn = Jn + ( -expected[out, 0]*(math.log(predicted_n[out, 0])) - (1-expected[out, 0])*(math.log(1-predicted_n[out, 0])) )
+                        Jp = Jp/len(train)
+                        Jn = Jn/len(train)
+                        if j>0: # não atualiza pesos do bias com o termo de regularização
+                            Jp = Jp + (self.struct['lambda']/(2*len(train)))*reg_p
+                            Jn = Jn + (self.struct['lambda']/(2*len(train)))*reg_n
+                        gradients[k][i,j] = (Jp-Jn)/(2*self.epsilon)
+        return gradients
     
-    def print_network (self):
+    def regularized_terms(self, k, i, j):
+        reg_p=0
+        reg_n=0
+        for k2 in range(self.layers-2, -1, -1):
+            #print(self.thetas_numerical[k])
+            for i2 in range(0, len(self.thetas_numerical[k2])):
+                #print("termo ignorado = " + str(self.thetas_numerical[k2][i2,0]))
+                for j2 in range(1, self.thetas_numerical[k2][i2].size): # começa em 1 para não somar os pesos de bias
+                    #print("\ttermo atual = " + str(self.thetas_numerical[k2][i2,j2]))
+                    if k2==k and i2==i and j2==j:
+                        reg_n = reg_n + pow(self.thetas_numerical[k2][i2,j2]-self.epsilon, 2)
+                        reg_p = reg_p + pow(self.thetas_numerical[k2][i2,j2]+self.epsilon, 2)
+                    else:
+                        reg_n = reg_n + pow(self.thetas_numerical[k2][i2,j2], 2)
+                        reg_p = reg_p + pow(self.thetas_numerical[k2][i2,j2], 2)
+        return reg_p, reg_n
+                        
+
+    def propagate_numerical(self, instance, thetas, k, i, j, op):
+        # adiciona bias
+        self.a_numerical[0] = np.matrix([1] + instance).transpose()
+        if op == "+":
+            thetas[k][i,j] = thetas[k][i,j] + self.epsilon
+        elif op == "-":
+            thetas[k][i,j] = thetas[k][i,j] - self.epsilon
+        for layer in range(len(thetas) - 1):
+            self.a_numerical[layer + 1] = g(thetas[layer] * self.a_numerical[layer])
+            self.a_numerical[layer + 1] = np.matrix([[1]] + self.a_numerical[layer + 1].tolist())               
+        self.a_numerical[self.layers-1] = g(thetas[self.layers - 2] * self.a_numerical[self.layers - 2])
+        if op == "+":
+            thetas[k][i,j] = thetas[k][i,j] - self.epsilon
+        elif op == "-":
+            thetas[k][i,j] = thetas[k][i,j] + self.epsilon
+        return self.a_numerical[self.layers-1]
+
+    def propagate_numerical_without_bias(self, instance, thetas, k, i, j, op):
+        if op == "+":
+            thetas[k][i,j] = thetas[k][i,j] + self.epsilon
+        elif op == "-":
+            thetas[k][i,j] = thetas[k][i,j] - self.epsilon
+        self.a_numerical[0] = np.matrix(instance).transpose()
+        for layer in range(len(thetas) - 1):                   
+            self.a_numerical[layer + 1] = g(thetas[layer] * self.a_numerical[layer])                 
+        self.a_numerical[self.layers-1] = g(thetas[self.layers - 2] * self.a_numerical[self.layers - 2])         
+        if op == "+":
+            thetas[k][i,j] = thetas[k][i,j] - self.epsilon
+        elif op == "-":
+            thetas[k][i,j] = thetas[k][i,j] + self.epsilon
+        return self.a_numerical[self.layers-1]
+    
+    def print_network(self):
         print ("Rede com " + str(self.layers) +  " camadas:\n") 
         layer = 0
         for n in self.struct['neurons']:
@@ -132,6 +214,41 @@ class NeuralNetwork:
             print (self.thetas[theta])
         
         print ("\n\n")
+    
+    def print_numerical_verification(self, numerical_gradients):
+        for k in range(0, len(numerical_gradients)):
+            print("\tGradiente numerico de Theta" + str(k+1) + ":")
+            for i in range(0, len(numerical_gradients[k])):
+                print("\t\t", end="")
+                for j in range(0, numerical_gradients[k][i].size):
+                    print("{:.5f}".format(numerical_gradients[k][i, j]), end=" ")
+                print("")
+            print("")
+        print("--------------------------------------------")
+
+    def backpropagation_numerically_verified(self, train):
+        b_grad = self.backpropagation(train)
+        n_grad = self.compute_numerical_verification(train)
+        self.print_numerical_verification(n_grad)
+        print("Verificando corretude dos gradientes com base nos gradientes numericos:")
+        #print(b_grad)
+        backprop = []
+        for layer in range(0, len(b_grad)):
+            sum = 0
+            for next in range(0, len(b_grad[layer])): #escolhe linha da matriz, que representa o neuronio onde os pesos da linha vão chegar
+                for weight in range(0, len(b_grad[layer][next][0])):
+                    sum = sum + b_grad[layer][next][0][weight]
+            backprop.append(sum)
+        #print(backprop)
+        for k in range(0, len(n_grad)):
+            print("\tErro entre gradiente via backprop e gradiente numerico para Theta" + str(k+1) + ": ", end="")
+            numerical = 0
+            for i in range(0, len(n_grad[k])):
+                for j in range(0, n_grad[k][i].size):                    
+                    numerical = numerical + n_grad[k][i,j]
+            print(numerical)
+            print(pow(backprop[k], 2) - pow(2,numerical))
+
 
 s = {
     'lambda' : 0.25,
@@ -163,6 +280,14 @@ train = [
     [[0.83, 0.02],[0.75, 0.28]]
 ]
 
-nn = NeuralNetwork(s, w)
 
+epsilon = 0.000001
+
+nn = NeuralNetwork(s, w, epsilon)
+            
 nn.backpropagation(train)
+numerical_gradients = nn.compute_numerical_verification(train)
+nn.print_numerical_verification(numerical_gradients)
+#nn.backpropagation_numerically_verified(train) #nao faco a menor ideia de como eles tao fazendo a comparacao
+#print("\n\n")
+#nn.print_network()
